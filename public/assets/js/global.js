2e63ac83605e26cda1ce5049594b67898ed2f5f1
theinfamouscoder5 <theinfamouscoder5@gmail.com>
2026-05-07 17:17:40 +0000

    basic setup

---
+++ public/assets/js/global.js
@@ -0,0 +1,41 @@
+// Helper: set cookie
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+// Helper: get cookie
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length);
+    }
+    return null;
+}
+
+$(document).ready(function () {
+
+    // Show banner only if cookie not set
+    if (!getCookie("cookieConsent")) {
+        setTimeout(function () {
+            $("#cookieConsent")
+                .fadeIn(200)
+                .css("display", "flex");
+        }, 500);
+    }
+
+    // Accept cookies (class selector)
+    $(".cookieConsentOK").on("click", function () {
+        setCookie("cookieConsent", "true", 365);
+        $("#cookieConsent").fadeOut(200);
+    });
+
+});
